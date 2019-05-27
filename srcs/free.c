b2c05513071dad6d0cb46ec63ebe7883565d9a58
Romain LECART <rlecart@e2r6p11.42.fr>
2019-05-27 20:24:55 +0200

    [rlecart]  - On va ptetre commit avant de refaire une betise hein (stash pop c la vi)

--- srcs/free.c
+++ srcs/free.c
@@ -6,14 +6,39 @@
 /*   By: pbernier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 14:43:26 by pbernier          #+#    #+#             */
-/*   Updated: 2019/05/06 14:43:28 by pbernier         ###   ########.fr       */
+/*   Updated: 2019/05/27 20:22:52 by rlecart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <malloc.h>
 
+void	*is_our(void *ptr)
+{
+	int		i;
+	void	*ret;
+	void	*page[4];
+
+	//page[0] = &;
+	return (ret);
+}
+
+void	*find_true(void *ptr)
+{
+	void	*ret;
+
+	return (ret);
+}
+
 void	free(void *ptr)
 {
-	//fucion des blocs
-	(void)ptr;
+	void	*cursor;
+	size_t	type;
+
+	//bzero(ptr, size);
+	if (ptr && (cursor = is_our(ptr)))
+	{
+		if (!CURSOR->empty)
+		{
+		}
+	}
 }
